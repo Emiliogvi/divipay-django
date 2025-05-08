5ba03c0c2aaa9ef3923a0b1047ace0d979496e03
Emilio Gomez <emilio.gomez@gmail.com>
2025-05-07 23:28:04 -0500

    Usar variables de entorno en settings.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn divipay.wsgi
--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-56@y$8wbxce+si34iw_qtvkk6j9&#((-=cchhhjay2gl#l*v%a'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'clave_insegura_de_emergencia')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
 
 
 # Application definition
